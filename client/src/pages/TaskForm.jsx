844a16c28f47bfbdcf99e6cdce3c90b121672183
mozart953 <wilsonnianopop@gmail.com>
2023-03-05 18:07:07 -0600

    communication frontend with backend

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,6 +1,58 @@
-function TaskForm(){
-    return(
-        <div>TaskForm</div>
+import { Form, Formik } from 'formik';
+import { createTaskRequest } from '../api/tasks.api.js'
+
+
+function TaskForm() {
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    title: "",
+                    description: "",
+
+
+                }}
+                onSubmit={async (values,actions) => {
+
+
+                    try {
+                        const response = await createTaskRequest(values);
+                        console.log(response);
+                        actions.resetForm();
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                }}
+            >
+                {({ handleChange, handleSubmit, values, isSubmitting }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <label>title</label>
+                        <input type="text" name='title'
+                            placeholder='Write a type'
+                            onChange={handleChange} 
+                            value={values.title}
+                            />
+
+                        <label>description</label>
+                        <textarea
+                            name='description'
+                            rows='3'
+                            placeholder='Write a description'
+                            onChange={handleChange}
+                            value = {values.description}
+                        ></textarea>
+
+                        <button type='submit' disabled={isSubmitting}>
+                            {isSubmitting ? "Saving..." : "Save"}
+                        </button>
+
+                    </Form>
+
+                )}
+
+            </Formik>
+        </div>
     )
 }
 
